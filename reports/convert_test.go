c59f96fb957cbac56e5ada735f583988223f3694
clambin <christophe.lambin@gmail.com>
2021-10-06 00:16:58 +0200

    fix: fix Y axis for Classification graph

--- reports/convert_test.go
+++ reports/convert_test.go
@@ -10,6 +10,11 @@ import (
 func TestMeasurementToGrid(t *testing.T) {
 	measurements := []store.Measurement{
 		{
+			Timestamp: time.Date(2021, 10, 5, 0, 0, 0, 0, time.UTC),
+			Intensity: 100,
+			Power:     2000,
+		},
+		{
 			Timestamp: time.Date(2021, 10, 5, 23, 59, 59, 0, time.UTC),
 			Intensity: 100,
 			Power:     4000,
@@ -20,5 +25,6 @@ func TestMeasurementToGrid(t *testing.T) {
 	c, r := grid.Dims()
 	assert.Equal(t, 24, c)
 	assert.Equal(t, 21, r)
+	assert.Equal(t, 2000.0, grid.Z(0, r-1))
 	assert.Equal(t, 4000.0, grid.Z(c-1, r-1))
 }
